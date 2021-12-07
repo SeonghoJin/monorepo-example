d7b5dba078fa27dccb845385e7516bb53970fa5b
moretall <seongho4996@naver.com>
2021-12-07 12:30:31 +0900

    change files

--- packages/d/index.js
+++ packages/d/index.js
@@ -1,4 +1,4 @@
-import {a} from 'a';
-import {b} from 'b';
-import {c} from 'c';
+import {a} from '@moretall/mono-example-a';
+import {b} from '@moretall/mono-example-b';
+import {c} from '@moretall/mono-example-c';
 console.log(a, b, c);
